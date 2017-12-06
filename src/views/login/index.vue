fe05991415dc2bc62d6d782fc5ed853bc26fa7ea
Pt丶血鬼 <747751012@qq.com>
2017-12-06 10:04:11 +0800

    项目初始化

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login-container">
+dfasd
+    </div>
+</template>
+
+<style type="stylesheet/scss" lang="scss">
+  $bg: #2d3a4b;
+  .login-container{
+    width: 100%;
+    height:100%;
+    background: $bg;
+    height: 100vh;
+    
+  }
+</style>
+
